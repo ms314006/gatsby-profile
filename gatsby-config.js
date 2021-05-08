1fe7d045b6935c3542d1d04db4ea59c987fe8869
GQSM <ms314006@hotmail.com>
2021-05-08 23:32:42 +0800

    change pwa icon to jpg and square

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,7 +89,7 @@ module.exports = {
         background_color: '#f7f0eb',
         theme_color: '#a2466c',
         display: 'standalone',
-        icon: 'src/pwaIcon.png',
+        icon: 'src/pwa-icon.jpg',
       },
     },
     'gatsby-plugin-offline',
